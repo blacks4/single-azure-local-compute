abd5cfc498a779e0ca10e13a81d9e04925f8d37e
Steve Tractenberg <steve@blacks4.com>
2026-08-24 00:04:59 -0400

    updated outputs

--- vault_and_less_var/call_module/outputs.tf
+++ vault_and_less_var/call_module/outputs.tf
@@ -1,24 +1,29 @@
-output "azure_local_vm_ids" {
-  description = "Resource IDs of created Azure Local VM instances keyed by hostname."
-  value       = { for hostname, vm in module.azure_local_vm : hostname => vm.azure_local_vm_id }
-}
+# output "azure_local_vm_ids" {
+#   description = "Resource IDs of created Azure Local VM instances keyed by hostname."
+#   value       = { for hostname, vm in module.azure_local_vm : hostname => vm.azure_local_vm_id }
+# }
 
-output "azure_local_vm_nic_ids" {
-  description = "Resource IDs of created Azure Local NICs keyed by hostname."
-  value       = { for hostname, vm in module.azure_local_vm : hostname => vm.azure_local_vm_nic_id }
-}
+# output "azure_local_vm_nic_ids" {
+#   description = "Resource IDs of created Azure Local NICs keyed by hostname."
+#   value       = { for hostname, vm in module.azure_local_vm : hostname => vm.azure_local_vm_nic_id }
+# }
 
-output "arc_machine_ids" {
-  description = "Resource IDs of created Arc machines keyed by hostname."
-  value       = { for hostname, vm in module.azure_local_vm : hostname => vm.arc_machine_id }
-}
+# output "arc_machine_ids" {
+#   description = "Resource IDs of created Arc machines keyed by hostname."
+#   value       = { for hostname, vm in module.azure_local_vm : hostname => vm.arc_machine_id }
+# }
 
-output "vm_ad_domains" {
-  description = "AD domain provided for each VM keyed by hostname."
-  value       = { for hostname, vm in module.azure_local_vm : hostname => vm.ad_domain }
-}
+# output "vm_ad_domains" {
+#   description = "AD domain provided for each VM keyed by hostname."
+#   value       = { for hostname, vm in module.azure_local_vm : hostname => vm.ad_domain }
+# }
 
-output "vm_hostname_and_ad_domain" {
-  description = "Hostname and AD domain provided for each VM keyed by hostname."
-  value       = { for hostname, vm in module.azure_local_vm : hostname => vm.vm_hostname_and_ad_domain }
+output "vm_details" {
+  description = "IP address and AD domain for each VM keyed by hostname."
+  value = {
+    for hostname, node in var.compute_nodes : hostname => {
+      ip_address = node.private_ip
+      ad_domain  = node.ad_domain
+    }
+  }
 }
